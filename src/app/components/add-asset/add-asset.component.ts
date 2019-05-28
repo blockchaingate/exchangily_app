bd5af1b8971fb86d6fdcf092aee257e81b28d496
unknown <ankit@fa.biz>
2019-05-28 12:08:58 -0400

    added coinlist in the UI

--- src/app/components/add-asset/add-asset.component.ts
+++ src/app/components/add-asset/add-asset.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Page } from 'tns-core-modules/ui/page';
 import { Router } from '@angular/router';
 import { android } from 'tns-core-modules/application';
@@ -14,16 +14,52 @@ import { isAndroid } from 'tns-core-modules/platform';
 
 export class AddAssetComponent implements OnInit {
 
+    public coinList: Array<any> = [];
     constructor(private page: Page, private router: Router) {
         page.actionBarHidden = true;
-        if(isAndroid){
+        if (isAndroid) {
             let activity = android.startActivity || android.foregroundActivity;
         }
+
+        this.coinList.push(
+            {
+                name: "EXG",
+                available: 19203.29302012,
+                frozen: 2000,
+                value: 389310.28
+            },
+            {
+                name: "BTC",
+                available: 19203.29302012,
+                frozen: 0.0000,
+                value: 389310.28
+            },
+            {
+                name: "FAB",
+                available: 19203.29302012,
+                frozen: 100000.00,
+                value: 389310.28
+            },
+            {
+                name: "FAB",
+                available: 19203.29302012,
+                frozen: 100000.00,
+                value: 389310.28
+            },
+            {
+                name: "FAB",
+                available: 19203.29302012,
+                frozen: 100000.00,
+                value: 389310.28
+            }
+        )
+
     }
+    /**/
 
     ngOnInit() {
         console.log("add asset component")
-         console.log(this.page.getActualSize());
-         
+        console.log(this.page.getActualSize());
+
     }
 }
